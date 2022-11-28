b96a706dc46ec9d2964b88ad3e6209bce18f458b
CatarauCorina <corina_catarau@yahoo.ro>
2022-11-28 17:28:07 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+sudo apt-get install python3.8
+pip install virtualenv
+virtualenv sprites --python=python3.8
+source sprites/bin/activate
+python --version
+pip install -r requirements_all.txt
+python src/trainer.py --tag default --config clevr6.ym
