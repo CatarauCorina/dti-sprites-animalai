2f2e87160996f8833114a900433b378e6e655168
CatarauCorina <corina_catarau@yahoo.ro>
2022-11-28 14:39:05 +0200

    Dti sprites initial commit

---
+++ src/utils/generate_masks.py
@@ -0,0 +1,22 @@
+import numpy as np
+from PIL import Image
+from src.utils.path import DATASETS_PATH
+from torchvision import transforms
+
+
+def create_masks():
+    count = 0
+    max_masks = 70000
+    path = f'{DATASETS_PATH}\CLEVR_v1.0\CLEVR_v1.0'
+    blackblankimage = transforms.ToPILImage()(np.zeros(shape=[128, 128, 3], dtype=np.uint8))
+    while count < max_masks:
+        blackblankimage.save(f'{path}/sem_masks/{count}.png')
+        count+=1
+
+
+def main():
+    create_masks()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/utils/path.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+# Project and source files
+PROJECT_PATH = Path(__file__).parent.parent.parent
+CONFIGS_PATH = PROJECT_PATH / 'configs'
+DATASETS_PATH = Path(__file__).parent.parent.parent.parent
+RUNS_PATH = PROJECT_PATH / 'runs'
+RESULTS_PATH = PROJECT_PATH / 'results'
