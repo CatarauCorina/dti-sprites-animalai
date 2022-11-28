5d3614eb600894fc32d553fec519184555201c7a
CatarauCorina <corina_catarau@yahoo.ro>
2022-11-28 17:34:05 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
-sudo apt-get install python3.8
-pip install virtualenv
-virtualenv sprites --python=python3.8
-source sprites/bin/activate
-python --version
-pip install -r requirements_all.txt
-python src/trainer.py --tag default --config clevr6.ym
+sudo apt-get update -y;
+sudo apt-get install python3.8;
+pip install virtualenv;
+virtualenv sprites --python=python3.8;
+source sprites/bin/activate;
+python --version;
+pip install -r requirements_all.txt;
+python src/trainer.py --tag default --config clevr6.yml;
