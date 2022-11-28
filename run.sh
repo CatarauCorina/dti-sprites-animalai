bdee728cf0911faf25576264eb08a26dc9903046
CatarauCorina <corina_catarau@yahoo.ro>
2022-11-28 18:43:12 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 sudo add-apt-repository ppa:deadsnakes/ppa;
 sudo apt-get update;
 sudo apt-get install python3.8;
-
 pip install virtualenv;
 virtualenv sprites --python=python3.8;
 source sprites/bin/activate;
@@ -9,4 +8,5 @@ python --version;
 pip install seaborn;
 pip install -r requirements_all.txt;
 python --version;
+pip freeze;
 python src/trainer.py --tag default --config clevr6.yml;
