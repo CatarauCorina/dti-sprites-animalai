53fdb4bc1314280ce58271f219fd1ff6149944ad
CatarauCorina <corina_catarau@yahoo.ro>
2022-11-28 18:32:32 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,7 @@ pip install virtualenv;
 virtualenv sprites --python=python3.8;
 source sprites/bin/activate;
 python --version;
+pip install seaborn;
 pip install -r requirements_all.txt;
+python --version;
 python src/trainer.py --tag default --config clevr6.yml;
