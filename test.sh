d9c4675c667d853f9682e61fcd9989ccf311bcbb
CatarauCorina <corina_catarau@yahoo.ro>
2022-11-29 16:08:19 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,4 @@ sudo add-apt-repository ppa:deadsnakes/ppa;
 sudo apt-get update;
 sudo apt-get install python3.8;
 pip install virtualenv;
-virtualenv test --python=python3.8;
-source test/bin/activate && pip -V;
-pip -V;
+source /content/drive/MyDrive/sprites/test/bin/activate || virtualenv test --python=python3.8 && pip -V;
