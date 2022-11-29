bc7241b7e8ef234a80e7416ee39499acf114cbe7
CatarauCorina <corina_catarau@yahoo.ro>
2022-11-29 19:28:05 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,5 +2,6 @@ sudo add-apt-repository ppa:deadsnakes/ppa;
 sudo apt-get update;
 sudo apt-get install python3.8;
 pip install virtualenv;
+chmod +x /content/drive/MyDrive/sprites/sprites/bin/pip
 chmod +x /content/drive/MyDrive/sprites/sprites/bin/python
 source /content/drive/MyDrive/sprites/sprites/bin/activate || virtualenv sprites --python=python3.8 && pip -V;
