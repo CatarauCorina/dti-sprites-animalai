f4cdc3a37b84d336c779f4ce0050e9039914c0f7
CatarauCorina <corina_catarau@yahoo.ro>
2022-11-29 16:03:55 +0200

    test script

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 sudo add-apt-repository ppa:deadsnakes/ppa;
 sudo apt-get update;
 sudo apt-get install python3.8;
+pip install virtualenv;
 virtualenv test --python=python3.8;
 source test/bin/activate && pip -V;
 pip -V;
